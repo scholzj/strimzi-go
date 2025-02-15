695826d6edbedc3eb8f89cde918d300d118c35ad
Jakub Scholz <www@scholzj.com>
2025-02-15 22:59:08 +0100

    Clean up tests
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- tests/clients/kafkatopic_test.go
+++ tests/clients/kafkatopic_test.go
@@ -2,51 +2,22 @@ package clients
 
 import (
 	"context"
-	"log"
-	"path/filepath"
+	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-
 	kafkav1beta2 "github.com/scholzj/strimzi-go/pkg/apis/kafka.strimzi.io/v1beta2"
-	strimzi "github.com/scholzj/strimzi-go/pkg/client/clientset/versioned"
 	strimziinformer "github.com/scholzj/strimzi-go/pkg/client/informers/externalversions"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
 )
 
-const NAMESPACE = "default"
-
-func GetConfig() (*rest.Config, error) {
-	var kubeconfig string
-
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = filepath.Join(home, ".kube", "config")
-	} else {
-		kubeconfig = ""
-	}
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	return config, err
-}
-
-func TestKafkaTopicCreateUpdateDeleteTest(t *testing.T) {
-	config, err := GetConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	clientset, err := strimzi.NewForConfig(config)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	newTopic := &kafkav1beta2.KafkaTopic{
+func NewResource() *kafkav1beta2.KafkaTopic {
+	return &kafkav1beta2.KafkaTopic{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "my-test-topic",
+			Name:      NAME,
+			Namespace: NAMESPACE,
 		},
 		Spec: &kafkav1beta2.KafkaTopicSpec{
 			Replicas:   3,
@@ -54,110 +25,116 @@ func TestKafkaTopicCreateUpdateDeleteTest(t *testing.T) {
 			Config:     kafkav1beta2.MapStringObject{"retention.ms": 7200000, "segment.bytes": 1073741824},
 		},
 	}
+}
 
-	// Create the topic
-	_, err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Create(context.TODO(), newTopic, metav1.CreateOptions{})
+func UpdatedResource(oldResource *kafkav1beta2.KafkaTopic) *kafkav1beta2.KafkaTopic {
+	updatedResource := oldResource.DeepCopy()
+
+	updatedResource.Spec.Replicas = 1
+	updatedResource.Spec.Partitions = 10
+	updatedResource.Spec.Config["segment.bytes"] = 107374182
+
+	return updatedResource
+}
+
+func AssertResource(t *testing.T, r1, r2 *kafkav1beta2.KafkaTopic) {
+	assert.Equal(t, r1.ObjectMeta.Name, r2.ObjectMeta.Name)
+	assert.Equal(t, r1.ObjectMeta.Namespace, r2.ObjectMeta.Namespace)
+	assert.Equal(t, r1.Spec.Replicas, r2.Spec.Replicas)
+	assert.Equal(t, r1.Spec.Replicas, r2.Spec.Replicas)
+	assert.Equal(t, r1.Spec.Partitions, r2.Spec.Partitions)
+	assert.EqualValues(t, r1.Spec.Config["retention.ms"], r2.Spec.Config["retention.ms"])
+	assert.EqualValues(t, r1.Spec.Config["segment.bytes"], r2.Spec.Config["segment.bytes"])
+}
+
+func TestKafkaTopicCreateUpdateDelete(t *testing.T) {
+	client := Client(t)
+
+	// Delete at the end to avoid errors
+	defer client.KafkaV1beta2().KafkaTopics(NAMESPACE).Delete(context.TODO(), NAME, metav1.DeleteOptions{})
+
+	// Create the resource
+	newResource := NewResource()
+	_, err := client.KafkaV1beta2().KafkaTopics(NAMESPACE).Create(context.TODO(), newResource, metav1.CreateOptions{})
 	if err != nil {
-		t.Fatalf("Failed to create topic: %s", err.Error())
+		t.Fatalf("Failed to create actualResource: %s", err.Error())
 	}
 
-	// Get the topic
-	topic, err := clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Get(context.TODO(), "my-test-topic", metav1.GetOptions{})
+	// Get the resource
+	actualResource, err := client.KafkaV1beta2().KafkaTopics(NAMESPACE).Get(context.TODO(), NAME, metav1.GetOptions{})
 	if err != nil {
-		t.Fatalf("Failed to get topic: %s", err.Error())
+		t.Fatalf("Failed to get actualResource: %s", err.Error())
 	}
 
-	// Assert the topic
-	if topic.Spec.Partitions != 3 || topic.Spec.Replicas != 3 || topic.Spec.Config["retention.ms"] != int64(7200000) || topic.Spec.Config["segment.bytes"] != int64(1073741824) {
-		t.Fatalf("Topic does not look correct: %v", topic)
-	}
+	// Assert the resource
+	AssertResource(t, newResource, actualResource)
 
-	// Update the topic
-	updatedTopic := topic.DeepCopy()
-	updatedTopic.Spec.Replicas = 1
-	updatedTopic.Spec.Partitions = 10
-	updatedTopic.Spec.Config["segment.bytes"] = 107374182
-
-	_, err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Update(context.TODO(), updatedTopic, metav1.UpdateOptions{})
+	// Update the resource
+	updatedResource := UpdatedResource(actualResource)
+	_, err = client.KafkaV1beta2().KafkaTopics(NAMESPACE).Update(context.TODO(), updatedResource, metav1.UpdateOptions{})
 	if err != nil {
-		t.Fatalf("Failed to update topic: %s", err.Error())
+		t.Fatalf("Failed to update actualResource: %s", err.Error())
 	}
 
-	// Get the topic
-	topic, err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Get(context.TODO(), "my-test-topic", metav1.GetOptions{})
+	// Get the resource
+	actualResource, err = client.KafkaV1beta2().KafkaTopics(NAMESPACE).Get(context.TODO(), NAME, metav1.GetOptions{})
 	if err != nil {
-		t.Fatalf("Failed to get topic: %s", err.Error())
+		t.Fatalf("Failed to get actualResource: %s", err.Error())
 	}
 
-	// Assert the topic
-	if topic.Spec.Partitions != 10 || topic.Spec.Replicas != 1 || topic.Spec.Config["retention.ms"] != int64(7200000) || topic.Spec.Config["segment.bytes"] != int64(107374182) {
-		t.Fatalf("Topic does not look correct: %v", topic)
-	}
+	// Assert the resource
+	AssertResource(t, updatedResource, actualResource)
 
-	// Delete the topic
-	err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Delete(context.TODO(), "my-test-topic", metav1.DeleteOptions{})
+	// Delete the resource
+	err = client.KafkaV1beta2().KafkaTopics(NAMESPACE).Delete(context.TODO(), NAME, metav1.DeleteOptions{})
 	if err != nil {
-		t.Fatalf("Failed to delete topic: %s", err.Error())
+		t.Fatalf("Failed to delete actualResource: %s", err.Error())
 	}
 
 	// Check deletion
-	topic, err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Get(context.TODO(), "my-test-topic", metav1.GetOptions{})
+	actualResource, err = client.KafkaV1beta2().KafkaTopics(NAMESPACE).Get(context.TODO(), NAME, metav1.GetOptions{})
 	if err != nil {
-		if err.Error() != "kafkatopics.kafka.strimzi.io \"my-test-topic\" not found" {
-			t.Fatalf("Failed to get topic: %s", err.Error())
+		if !strings.Contains(err.Error(), "not found") {
+			t.Fatalf("Failed to get resource: %s", err.Error())
 		}
-	} else if topic != nil {
+	} else if actualResource != nil {
 		t.Fatalf("Topic still exists")
 	}
 }
 
 func TestKafkaTopicInformerAndLister(t *testing.T) {
-	config, err := GetConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
+	client := Client(t)
 
-	clientset, err := strimzi.NewForConfig(config)
-	if err != nil {
-		t.Fatal(err)
-	}
+	// Delete at the end to avoid errors
+	defer client.KafkaV1beta2().KafkaTopics(NAMESPACE).Delete(context.TODO(), NAME, metav1.DeleteOptions{})
 
+	// Setup informer
 	added := 0
 	addedSignal := make(chan struct{})
 	defer close(addedSignal)
-	onAdd := func(new interface{}) {
-		newKt := new.(*kafkav1beta2.KafkaTopic)
-		log.Printf("New topic %s in namespace %s", newKt.Name, newKt.Namespace)
-		added++
-		addedSignal <- struct{}{}
-	}
 	updatedSignal := make(chan struct{})
 	defer close(updatedSignal)
 	updated := 0
-	onUpdate := func(old interface{}, new interface{}) {
-		newKt := new.(*kafkav1beta2.KafkaTopic)
-		oldKt := old.(*kafkav1beta2.KafkaTopic)
-		log.Printf("Updated topic %s in namespace %s", newKt.Name, oldKt.Namespace)
-		updated++
-		updatedSignal <- struct{}{}
-	}
 	deletedSignal := make(chan struct{})
 	defer close(deletedSignal)
 	deleted := 0
-	onDelete := func(old interface{}) {
-		oldKt := old.(*kafkav1beta2.KafkaTopic)
-		log.Printf("Deleted topic %s in namespace %s", oldKt.Name, oldKt.Namespace)
-		deleted++
-		deletedSignal <- struct{}{}
-	}
 
 	// Create informer and lister
-	factory := strimziinformer.NewSharedInformerFactoryWithOptions(clientset, time.Hour*1)
+	factory := strimziinformer.NewSharedInformerFactoryWithOptions(client, time.Hour*1)
 	informer := factory.Kafka().V1beta2().KafkaTopics()
-	_, err = informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    onAdd,
-		UpdateFunc: onUpdate,
-		DeleteFunc: onDelete,
+	_, err := informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(new interface{}) {
+			added++
+			addedSignal <- struct{}{}
+		},
+		UpdateFunc: func(old interface{}, new interface{}) {
+			updated++
+			updatedSignal <- struct{}{}
+		},
+		DeleteFunc: func(old interface{}) {
+			deleted++
+			deletedSignal <- struct{}{}
+		},
 	})
 	if err != nil {
 		t.Fatalf("Failed to create informer: %s", err.Error())
@@ -172,88 +149,74 @@ func TestKafkaTopicInformerAndLister(t *testing.T) {
 
 	lister := informer.Lister()
 
-	// Create the topic
-	newTopic := &kafkav1beta2.KafkaTopic{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "my-test-topic2",
-		},
-		Spec: &kafkav1beta2.KafkaTopicSpec{
-			Replicas:   3,
-			Partitions: 3,
-			Config:     kafkav1beta2.MapStringObject{"retention.ms": 7200000, "segment.bytes": 1073741824},
-		},
-	}
-
-	_, err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Create(context.TODO(), newTopic, metav1.CreateOptions{})
+	// Create the resource
+	newResource := NewResource()
+	_, err = client.KafkaV1beta2().KafkaTopics(NAMESPACE).Create(context.TODO(), newResource, metav1.CreateOptions{})
 	if err != nil {
 		t.Fatalf("Failed to create topic: %s", err.Error())
 	}
 
+	// Wait for the resource to be added in the informer
 	<-addedSignal
 
-	// Get the topic
-	topic, err := lister.KafkaTopics(NAMESPACE).Get("my-test-topic2")
+	// Get the resource
+	actualResource, err := lister.KafkaTopics(NAMESPACE).Get(NAME)
 	if err != nil {
 		t.Fatalf("Failed to get topic: %s", err.Error())
 	}
 
-	// Assert the topic
-	if topic.Spec.Partitions != 3 || topic.Spec.Replicas != 3 || topic.Spec.Config["retention.ms"] != int64(7200000) || topic.Spec.Config["segment.bytes"] != int64(1073741824) {
-		t.Fatalf("Topic does not look correct: %v", topic)
-	}
+	// Assert the resource
+	AssertResource(t, newResource, actualResource)
 
-	// Update the topic
-	updatedTopic := topic.DeepCopy()
-	updatedTopic.Spec.Replicas = 1
-	updatedTopic.Spec.Partitions = 10
-	updatedTopic.Spec.Config["segment.bytes"] = 107374182
-
-	_, err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Update(context.TODO(), updatedTopic, metav1.UpdateOptions{})
+	// Update the resource
+	updatedResource := UpdatedResource(actualResource)
+	_, err = client.KafkaV1beta2().KafkaTopics(NAMESPACE).Update(context.TODO(), updatedResource, metav1.UpdateOptions{})
 	if err != nil {
 		t.Fatalf("Failed to update topic: %s", err.Error())
 	}
 
+	// Wait for resource to be updated in the informer
 	<-updatedSignal
 
 	// Get the topic
-	topic, err = lister.KafkaTopics(NAMESPACE).Get("my-test-topic2")
+	actualResource, err = lister.KafkaTopics(NAMESPACE).Get(NAME)
 	if err != nil {
 		t.Fatalf("Failed to get topic: %s", err.Error())
 	}
 
-	// Assert the topic
-	if topic.Spec.Partitions != 10 || topic.Spec.Replicas != 1 || topic.Spec.Config["retention.ms"] != int64(7200000) || topic.Spec.Config["segment.bytes"] != int64(107374182) {
-		t.Fatalf("Updated topic does not look correct: %v", topic)
-	}
+	// Assert the resource
+	AssertResource(t, updatedResource, actualResource)
 
-	// Delete the topic
-	err = clientset.KafkaV1beta2().KafkaTopics(NAMESPACE).Delete(context.TODO(), "my-test-topic2", metav1.DeleteOptions{})
+	// Delete the resource
+	err = client.KafkaV1beta2().KafkaTopics(NAMESPACE).Delete(context.TODO(), NAME, metav1.DeleteOptions{})
 	if err != nil {
 		t.Fatalf("Failed to delete topic: %s", err.Error())
 	}
 
+	// Wait for resource to be deleted
 	<-deletedSignal
 
 	// Check deletion
-	topic, err = lister.KafkaTopics(NAMESPACE).Get("my-test-topic2")
+	actualResource, err = lister.KafkaTopics(NAMESPACE).Get(NAME)
 	if err != nil {
-		if err.Error() != "kafkatopics.kafka.strimzi.io \"my-test-topic2\" not found" {
-			// pass
-		} else {
-			t.Fatalf("Failed to get topic: %s", err.Error())
+		if !strings.Contains(err.Error(), "not found") {
+			t.Fatalf("Failed to get resource: %s", err.Error())
 		}
-	} else if topic != nil {
-		t.Fatalf("Topic still exists")
+	} else if actualResource != nil {
+		t.Fatalf("Resource still exists")
 	}
 
 	// Assert the event handled
+	assert.Equal(t, added, 1)
+	assert.Equal(t, updated, 1)
+	assert.Equal(t, deleted, 1)
 	if added != 1 {
-		t.Fatalf("Topic was not added once but %d", added)
+		t.Fatalf("Resource was not added once but %d", added)
 	}
 	if updated != 1 {
-		t.Fatalf("Topic was not updated once but %d", updated)
+		t.Fatalf("Resource was not updated once but %d", updated)
 	}
 	if deleted != 1 {
-		t.Fatalf("Topic was not deleted once but %d", deleted)
+		t.Fatalf("Resource was not deleted once but %d", deleted)
 	}
 }
